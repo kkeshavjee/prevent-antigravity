d87c4cdbd998a89be5117884982c2126d2bd6bba
Karim Keshavjee <karim@infoclin.ca>
2026-01-10 11:04:12 -0500

    Remove nested Documents folder to fix duplication

--- Documents/Antigravity/run_frontend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Frontend Client...
-cd frontend
-npm run dev
-pause
--- Documents/Antigravity/setup_frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing Frontend Dependencies...
-cd frontend
-npm install
-echo.
-echo Installation Complete!
-echo You can now run 'run_frontend.bat' safely.
-pause
